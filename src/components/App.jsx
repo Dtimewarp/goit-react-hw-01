9b7095fc7cd91e08446f6d2bfb0b223646d4174b
Dtimewarp <dtimewarp@gmail.com>
2024-01-14 18:04:11 +0200

    add full files from "chernovik"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import { Product } from './Product'
+import './App.css';
+import { Profile } from './Profile/Profile';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import userData from '../data/userData.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
-
-export function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
-}
+};
+
+export { App };
